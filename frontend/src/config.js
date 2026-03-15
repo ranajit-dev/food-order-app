f854192faf40f04875565dd7ec8ff98e448cf1ac
ranajit-dev <rudraranajit944@gmail.com>
2026-03-15 19:57:42 +0530

    Update backend URL in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,6 +2,6 @@
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:8000"
-    : "https://food-order-backend.onrender.com";
+    : "https://food-order-backend-s5it.onrender.com";
 
 export { API_URL };
